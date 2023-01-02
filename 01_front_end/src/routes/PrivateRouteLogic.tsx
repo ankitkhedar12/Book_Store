27ea376da3a5b896b55124bd1f48314c2e81f5f0
Ankit Khedar <57590215+ankitkhedar12@users.noreply.github.com>
2023-01-02 16:10:43 +0530

    cfv

--- 01_front_end/src/routes/PrivateRouteLogic.tsx
+++ 01_front_end/src/routes/PrivateRouteLogic.tsx
@@ -1,5 +1,4 @@
-
-import { FunctionComponent, useEffect } from 'react';     
+import { useEffect } from 'react';     
 // import jwt_decode from "jwt-decode";
 import { isExpired } from "react-jwt";
 import { useSelector } from 'react-redux';
@@ -9,30 +8,12 @@ export function PrivateRouteLogic({ children }:any){
   let token: string = localStorage.getItem("token")!;
   const isMyTokenExpired = isExpired(token);
   const userIsLoggedIn: boolean = useSelector((state: any) => state.user.isLoggedIn);
-  // const accessToken = () => localStorage.getItem("token") as string;
-  // // const decodedToken: IDecodedToken = jwt_decode(accessToken);
+  /**  const accessToken = () => localStorage.getItem("token") as string;
+  * const decodedToken: IDecodedToken = jwt_decode(accessToken);  
+  * */
   useEffect(() => {
+    console.log("UserIsLoggedIn: ", userIsLoggedIn);
     token = localStorage.getItem("token")!;
   }, [isMyTokenExpired]);
-import { FunctionComponent, useState, useEffect } from 'react';   
-// import jwt_decode from "jwt-decode";
-import { Navigate, Route, useLocation } from "react-router-dom";
-// import { IDecodedToken } from "../interfaces/Interface";
-
-export function PrivateRouteLogic({ children }:any) {  
-  const [isLoggedIn, setLoggedIn ] =useState(false);
-
-  const accessToken = () => localStorage.getItem("token") as string;
-  // const decodedToken: IDecodedToken = jwt_decode(accessToken);
-  async function SetLoggedIn() {
-    console.log("token", accessToken());
-    if(accessToken()){
-      await setLoggedIn(true);
-      console.log("Inside UseEffect: : : ", isLoggedIn);
-    }
-  }
-  useEffect(()=>{
-    SetLoggedIn();
-  },[])
-   return (userIsLoggedIn) ? children : <Navigate to="/signin" />
+   return (token) ? children : <Navigate to="/signin" />
 };
